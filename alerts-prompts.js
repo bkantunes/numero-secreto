3e1fc6b186ea382e5642684191784f28f558cb45
bkantunes <bkarinesantos@hotmail.com>
2026-04-07 00:31:44 -0300

    funções

---
+++ alerts-prompts.js
@@ -0,0 +1,33 @@
+
+
+
+alert(`Boas vindas ao jogo do número secreto.`);
+let secretNumber = parseInt(Math.random() * 100 + 1);
+console.log(secretNumber);
+let guess;
+let tries = 1;
+console.log(guess);
+
+// lógica do jogo
+while (guess != secretNumber){
+    guess = prompt(`Escolha um número entre 1 e 100:`);
+    if (guess == secretNumber){
+    break;
+} else if ( guess > secretNumber){
+    alert(`O número secreto é menor que ${guess}.`);
+
+} else {
+    alert(`O número secreto é maior que ${guess}.`);
+}
+    tries++
+}
+
+let palavraTentativa = tries > 1? 'tentativas' : 'tentativa';
+
+alert(`Você acertou o número (${secretNumber}) secreto! com ${tries} ${palavraTentativa}.`);
+
+// if (tries > 1){
+//     alert(`Você acertou o número (${secretNumber}) secreto! com ${tries} tentativas.`);
+// } else {
+//     alert(`Você acertou o número (${secretNumber}) secreto! com ${tries} tentativa.`);
+// }
--- app.js
+++ app.js
@@ -1,30 +1,9 @@
-alert(`Boas vindas ao jogo do número secreto.`);
-let secretNumber = parseInt(Math.random() * 100 + 1);
-console.log(secretNumber);
-let guess;
-let tries = 1;
-console.log(guess);
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
 
-// lógica do jogo
-while (guess != secretNumber){
-    guess = prompt(`Escolha um número entre 1 e 100:`);
-    if (guess == secretNumber){
-    break;
-} else if ( guess > secretNumber){
-    alert(`O número secreto é menor que ${guess}.`);
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
-} else {
-    alert(`O número secreto é maior que ${guess}.`);
+function verificarChute() {
+    console.log('o botão foi clicado')
 }
-    tries++
-}
-
-let palavraTentativa = tries > 1? 'tentativas' : 'tentativa';
-
-alert(`Você acertou o número (${secretNumber}) secreto! com ${tries} ${palavraTentativa}.`);
-
-// if (tries > 1){
-//     alert(`Você acertou o número (${secretNumber}) secreto! com ${tries} tentativas.`);
-// } else {
-//     alert(`Você acertou o número (${secretNumber}) secreto! com ${tries} tentativa.`);
-// }
