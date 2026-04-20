156608dd2201bf594b296213c4e0b51640b51ea6
bkantunes <bkarinesantos@hotmail.com>
2026-04-20 11:53:17 -0300

    contagem de tentativas

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function gerarNumeroAleatorio(){
     numero = parseInt(Math.random() * 100 + 1);
@@ -17,17 +18,38 @@ function exibirTextoNaTela(tag, texto){
 exibirTextoNaTela('h1','Jogo do número secreto');
 exibirTextoNaTela('p','Escolha um número entre 1 e 100');
 
-console.log(numeroSecreto);
+
 function verificarChute() {
     let chute = document.querySelector('input').value;
     if (chute == numeroSecreto){
         exibirTextoNaTela('h1','Acertou!');
-        exibirTextoNaTela('p', 'Você descobriu o número secreto!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
     } else {
         if (chute > numeroSecreto){
             exibirTextoNaTela('p',`Errou... O número secreto é menor que ${chute}`);
         } else {
             exibirTextoNaTela('p',`Errou... O número secreto é maior que ${chute}`);
         }
+        tentativas++
     }
 }
+
+/*
+function contador(){
+    let chute = document.querySelector('input').value;
+    let tentativas = 1;
+    if (chute){
+        tentativa++
+    }
+    return tentativa;
+}
+
+let tentativa = contador();
+console.log(tentativa);
+*/
+
+
+
+
