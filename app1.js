30d65a866358d528f4c41bfb647050762af815de
bkantunes <bkarinesantos@hotmail.com>
2026-04-20 12:12:09 -0300

    contador total de tentativas

---
+++ app1.js
@@ -0,0 +1,51 @@
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 0;
+
+function gerarNumeroAleatorio(){
+    numero = parseInt(Math.random() * 100 + 1);
+    return numero;
+    
+}
+
+console.log(numeroSecreto);
+
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+
+}
+
+exibirTextoNaTela('h1','Jogo do número secreto');
+exibirTextoNaTela('p','Escolha um número entre 1 e 100');
+
+    function contador(){
+        tentativas++
+        console.log(tentativas);
+    }
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+
+    if (chute == numeroSecreto){
+        contador();
+        exibirTextoNaTela('h1','Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+    } else {
+        if (chute > numeroSecreto){
+            exibirTextoNaTela('p',`Errou... O número secreto é menor que ${chute}`);
+        } else {
+            exibirTextoNaTela('p',`Errou... O número secreto é maior que ${chute}`);
+        }
+        contador();
+    }
+ 
+}
+
+
+
+
+
+
+
