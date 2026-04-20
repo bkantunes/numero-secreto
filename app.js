d58c278438370d77a03e5b81cad8d29658c289e6
bkantunes <bkarinesantos@hotmail.com>
2026-04-20 14:43:24 -0300

    função reiniciar jogo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let numeroSecreto = gerarNumeroAleatorio();
-let tentativas = 1;
+let tentativas = 0;
 
 function gerarNumeroAleatorio(){
     numero = parseInt(Math.random() * 100 + 1);
@@ -15,40 +15,56 @@ function exibirTextoNaTela(tag, texto){
 
 }
 
-exibirTextoNaTela('h1','Jogo do número secreto');
-exibirTextoNaTela('p','Escolha um número entre 1 e 100');
+function exibirMsgInicial(){
+    exibirTextoNaTela('h1','Jogo do número secreto');
+    exibirTextoNaTela('p','Escolha um número entre 1 e 100');
+};
 
+exibirMsgInicial();
+
+    function contador(){
+        tentativas++
+        console.log(tentativas);
+    }
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
+
     if (chute == numeroSecreto){
+        contador();
         exibirTextoNaTela('h1','Acertou!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         if (chute > numeroSecreto){
             exibirTextoNaTela('p',`Errou... O número secreto é menor que ${chute}`);
         } else {
             exibirTextoNaTela('p',`Errou... O número secreto é maior que ${chute}`);
         }
-        tentativas++
+        contador();
+        limparCampo();
     }
+ 
 }
 
-/*
-function contador(){
-    let chute = document.querySelector('input').value;
-    let tentativas = 1;
-    if (chute){
-        tentativa++
-    }
-    return tentativa;
-}
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+};
 
-let tentativa = contador();
-console.log(tentativa);
-*/
+function reiniciarJogo(){
+    numeroSecreto = gerarNumeroAleatorio();
+    console.log(numeroSecreto);
+    limparCampo();
+    tentativas = 0;
+    exibirMsgInicial();
+    document.getElementById('reiniciar').setAttribute('disabled',true);
+};
+
+
+
 
 
 
